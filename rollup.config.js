ed105cfba6c6c88cfbeb0fe47f97599bf241b1f1
hardy22110 <hardy22110@gmail.com>
2023-03-14 08:30:31 +0800

    Adjust: output config (umd, es)

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,30 @@
 import typescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'src/index.ts',
   output: [
+    // 瀏覽器和 Node.js
     {
       file: 'dist/index.js',
       format: 'umd',
-      name: 'app-tools',
-      sourcemap: false,
-    },
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
+      name: 'appTools'
     },
     {
       file: 'dist/index.esm.js',
-      format: 'esm',
+      format: 'es',
+      name: 'appTools',
+      sourcemap: false
     },
+    {
+      file: 'dist/index.common.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: false
+    }
   ],
   plugins: [
     typescript({
@@ -26,15 +32,17 @@ export default {
       tsconfig: 'tsconfig.json',
       tsconfigOverride: {
         include: ['src/**/*.ts'],
-        exclude: ['tests/**/*.ts'],
-      },
+        exclude: ['tests/**/*.ts']
+      }
     }),
     babel({
       extensions: ['.js', '.ts'],
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
-      presets: ['@babel/preset-env', '@babel/preset-typescript'],
+      presets: ['@babel/preset-env', '@babel/preset-typescript']
     }),
     terser(),
-  ],
+    resolve(),
+    commonjs()
+  ]
 }
